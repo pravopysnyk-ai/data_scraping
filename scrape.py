3172ef91ccf6b5ef08186f32a80a1a50fbefe035
Artem Yushko <yushko2002@gmail.com>
2023-03-25 22:02:20 -0500

    scrape file tweaks

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from data_classes.WebScraper import WebScraper
 from data_classes.TextMerger import TextMerger
 import os
 
-def main(website_exports_folder="/data/raw_exports", output_path="data/output"):
+def main(website_exports_folder="./data/raw_exports", output_path=".data/output"):
     """
     Main function for getting the links from Telegram exports and getting the text from them.
     """
@@ -14,7 +14,7 @@ def main(website_exports_folder="/data/raw_exports", output_path="data/output"):
         os.makedirs(output_path)
     # scraping the text from each link
     for website_folder in os.listdir(website_exports_folder):
-        website_folder = website_exports_folder + website_folder
+        website_folder = website_exports_folder + "/" + website_folder
         htmls = web_scraper.get_html_from_folder(website_folder)
         urls = web_scraper.extract_urls(website_folder, htmls)
         lines = web_scraper.extract_text_from_urls(urls)
